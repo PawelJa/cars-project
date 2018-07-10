952f84fcb9c56c4430ad1e14886556b17485a88d
PawelJa <janecki.paw@gmail.com>
2018-07-10 14:41:41 +0200

    CORS enabled

--- src/main/java/com/cars/demo/repository/CarRepository.java
+++ src/main/java/com/cars/demo/repository/CarRepository.java
@@ -1,11 +1,13 @@
 package com.cars.demo.repository;
 
-import com.cars.demo.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.cars.demo.entity.Car;
 
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 }

85af4695fc0367e347c5ab464d0cc4b981ebfcf6
PawelJa <janecki.paw@gmail.com>
2018-07-11 11:49:39 +0200

    CRUD ability for car entity

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,33 @@ import { CarListComponent } from './car-list/car-list.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Browser } from 'protractor';
+import { componentFactoryName } from '@angular/compiler';
 import { GiphyService } from './shared/giphy/giphy.service';
+import { CarEditComponent } from './car-edit/car-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoute: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full'},
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +46,9 @@ import { GiphyService } from './shared/giphy/giphy.service';
     MatCardModule,
     MatInputModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(appRoute),
+    FormsModule,
   ],
   providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
